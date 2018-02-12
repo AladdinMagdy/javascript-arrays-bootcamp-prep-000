f3d38c93a07994530c24a069f3d1500a49fd582c
Aladdin Magdy <aladdinmagdysaber167@commerce.helwan.edu.eg>
2018-02-12 20:12:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,7 @@ function addElementToBeginningOfArray(array, element){
   var arr = [element, ...array];
  return arr;
 }
+<<<<<<< HEAD
 function destructivelyAddElementToBeginningOfArray(array, element){
  array.unshift(element);
  return array;
@@ -34,4 +35,10 @@ function destructivelyRemoveElementFromEndOfArray(array){
 function removeElementFromEndOfArray(array){
   
  return array.slice(0, array.length - 1);
+=======
+function addElementToBeginningOfArray(array, element){
+  
+  var arr = [element, ...array];
+ return arr;
+>>>>>>> 18f8b5acc8cdb648ece2d1d99e3cad08c7797e3c
 }
